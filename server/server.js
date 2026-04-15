577b861384ca4a7c355bc2ed5a4f2b4f458a0d8e
workholics-shikha <workholics.shikha@gmail.com>
2026-04-15 19:56:06 +0530

    seeder calling removd from serverJs file

--- server/server.js
+++ server/server.js
@@ -10,24 +10,14 @@ async function start() {
   const MONGODB_URI = requiredEnv("MONGODB_URI"); // ✅ correct
   const JWT_SECRET = requiredEnv("JWT_SECRET");
   const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "7d";
-  const seedAdmin = require("./src/seed/seedAdmin");
-
+  
   await connectDB(MONGODB_URI); // ✅ FIX HERE
 
   const app = createApp({
     jwtSecret: JWT_SECRET,
     jwtExpiresIn: JWT_EXPIRES_IN,
   });
-
-  app.get("/seed-admin", async (req, res) => {
-    try {
-      await seedAdmin(); // tumhara seeder function
-      res.send("✅ Admin Seeded");
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  });
-
+ 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
